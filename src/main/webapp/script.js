05f7e058bbcf6e18ba68bba77b2bbb80afab17a9
root <root@ip-172-31-82-132.ec2.internal>
2023-05-31 11:25:47 +0000

    correcting application code

---
+++ src/main/webapp/script.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code here
+// This is just a sample implementation
+// Feel free to modify it as per your requirements
+
+// Example: Display an alert when the form is submitted
+document.querySelector('form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Task submitted!');
+});
---
+++ target/student-task-tracker-1.0-SNAPSHOT/index.jsp
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Student Task Tracker</title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <h1>Welcome to the Student Task Tracker</h1>
+
+    <!-- Add your HTML content here -->
+    
+    <script src="script.js"></script>
+</body>
+</html>
